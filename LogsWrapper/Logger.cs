222fcce721675fad3e3fe74e3d4e99b11e4e3006
Roma <superdiadaroma@rambler.ru>
2019-03-29 13:36:44 +0300

    Сделал MVVM Wrapper

--- LogsWrapper/Logger.cs
+++ LogsWrapper/Logger.cs
@@ -6,21 +6,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using log4net;
 using log4net.Config;
-/*
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;*/
 
 /// <summary>
 /// Предназначено для размещения утилитных функционалов работы с логами.
@@ -57,7 +42,6 @@ namespace LogsWrapper
         public static void InitLogger()
         {
             XmlConfigurator.Configure();
-            //log = LogManager.GetLogger("LOGGER");
         }
         
         /// <summary>
---
+++ MVVMUtilsWrapper/NotifyPropertyChanged.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVMUtilsWrapper
+{
+    /// <summary>
+    /// Базовый класс для классов ViewModel с общим функционалом
+    /// </summary>
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
