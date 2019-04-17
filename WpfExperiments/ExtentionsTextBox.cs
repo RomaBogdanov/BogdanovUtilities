15024b2f5f94df773f9559af03fb038f6def6308
Roma <superdiadaroma@rambler.ru>
2019-04-17 12:01:01 +0300

    Фикс какого-то намеса.

---
+++ WpfExperiments/ExtentionsTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfExperiments
+{
+    public static class ExtentionsTextBox
+    {
+        public static readonly DependencyProperty HintProp = 
+            DependencyProperty.RegisterAttached(
+            "Hint", typeof(object), typeof(ExtentionsTextBox), 
+            new FrameworkPropertyMetadata(default(string), 
+            FrameworkPropertyMetadataOptions.Inherits));
+
+        /// <summary>
+        /// Sets the hint.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="value">The value.</param>
+        public static void SetHint(DependencyObject element, object value)
+        {
+            element.SetValue(HintProp, value);
+        }
+
+        /// <summary>
+        /// Gets the hint.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <returns>
+        /// The <see cref="string" />.
+        /// </returns>
+        public static object GetHint(DependencyObject element)
+        {
+            return element.GetValue(HintProp);
+        }
+    }
+}
---
+++ WpfExperiments/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfExperiments
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TextBox_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+        }
+
+        private void TextBox_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+        
+        private void TextBox_PreviewDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var a = e.Data.GetData(DataFormats.FileDrop);
+            }
+            if (Clipboard.ContainsFileDropList())
+            {
+                var a = Clipboard.GetFileDropList();
+            }
+            //else if( Clipboard.ContainsData())
+        }
+    }
+}
