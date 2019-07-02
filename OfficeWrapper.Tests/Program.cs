be8deac159271e54fbc3680ccf2bb649e6aa7345
Roma <superdiadaroma@rambler.ru>
2019-07-02 19:47:34 +0300

    Добавление проекта по работе с офисом.

---
+++ OfficeWrapper.Tests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace OfficeWrapper.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spreadsheet spreadsheet = new Spreadsheet();
+            DataTable dataTable = new DataTable("Тестовая");
+            dataTable.Columns.Add("Колонка1");
+            dataTable.Columns.Add("Колонка2");
+            dataTable.Rows.Add(new object[] { "1", "2" });
+            spreadsheet.CreateSpreadsheet(dataTable, "Test.xlsx");
+        }
+    }
+}
---
+++ OfficeWrapper/Spreadsheet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using GemBox;
+using GemBox.Spreadsheet;
+
+namespace OfficeWrapper
+{
+    /// <summary>
+    /// Обёртка для excell файлов.
+    /// </summary>
+    public class Spreadsheet
+    {
+        public Spreadsheet()
+        {
+            GemBox.Spreadsheet.SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+
+        /// <summary>
+        /// Процедура создания таблицы в excel из объекта DataTable
+        /// </summary>
+        /// <param name="dataTable">Объект данных для создания таблицы</param>
+        /// <param name="path">Путь к создаваемому файлу.</param>
+        public void CreateSpreadsheet(DataTable dataTable, string path)
+        {
+            var workbook = new ExcelFile();
+            string name = dataTable.TableName == "" ? "Неизвестно" : dataTable.TableName;
+            var worksheet = workbook.Worksheets.Add(name);
+            worksheet.InsertDataTable(dataTable);
+            workbook.Save(path);
+        }
+    }
+}
