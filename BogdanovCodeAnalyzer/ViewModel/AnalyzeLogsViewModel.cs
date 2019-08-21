bbf661164684b163aa73a35de6f897e70708730c
Roma <superdiadaroma@rambler.ru>
2019-08-21 19:45:21 +0300

    Небольшой рефакторинг.

---
+++ BogdanovCodeAnalyzer/ViewModel/AnalyzeLogsViewModel.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ServiceModel;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using BogdanovUtilitisLib.MVVMUtilsWrapper;
+
+namespace BogdanovCodeAnalyzer.ViewModel
+{
+    /// <summary>
+    /// Анализ файла логгирования.
+    /// </summary>
+    /// <remarks>
+    /// 1. Обработка файла логгирования:
+    /// v------ 1.1. Получение частотности входа в метод;
+    /// 1.2. Получение частотности использования класса;
+    /// v------ 1.3. Получение частотности использования файла;
+    /// 2. Сделать фильтрацию:
+    /// 2.1. По логам в методе;
+    /// 2.2. По логам в классе;
+    /// 2.3. По логам в файле;
+    /// </remarks>
+    class AnalyzeLogsViewModel : NotifyPropertyChanged
+    {
+        private string pathToAnalyzeFiles = @"C:\BogdanovR\Experiments\Sintez\";
+        private string textInTextBlock;
+        private string textAddedNamespase = "SintezLibrary";
+        //private string logFilesPath = @"C:\BogdanovR\Experiments\Sintez\SintezOSPClient\bin\Debug\Logs\Log.log";
+        private string logFilesPath = @"C:\BogdanovR\MyReps\BogdanovUtilities\BogdanovCodeAnalyzer\bin\Debug\Logs\Log.log";
+        private bool isFrequenceRepeatStrings;
+        private bool isFrequenceRepeatFiles;
+        private ObservableCollection<Log> logs;
+
+
+
+        /// <summary>
+        /// Информация по логам.
+        /// </summary>
+        public ObservableCollection<Log> Logs
+        {
+            get => logs;
+            set
+            {
+                logs = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Путь к файлам, которые будут покрываться логами.
+        /// </summary>
+        public string PathToAnalyzeFiles
+        {
+            get => pathToAnalyzeFiles;
+            set
+            {
+                pathToAnalyzeFiles = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Текст выводимый в текстовый блок.
+        /// </summary>
+        public string TextInTextBlock
+        {
+            get => textInTextBlock;
+            set
+            {
+                textInTextBlock = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Наименование добавляемого пространства имён.
+        /// </summary>
+        public string TextAddedNamespase
+        {
+            get => textAddedNamespase;
+            set
+            {
+                textAddedNamespase = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Путь к анализируемому лог-файлу.
+        /// </summary>
+        public string LogFilesPath
+        {
+            get => logFilesPath;
+            set
+            {
+                logFilesPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Флаг того, что ищется частота встречаемости строки в логах
+        /// </summary>
+        public bool IsFrequenceRepeatStrings
+        {
+            get => isFrequenceRepeatStrings;
+            set
+            {
+                isFrequenceRepeatStrings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Флаг того, что ищется частота встречаемости файлов в логах
+        /// </summary>
+        public bool IsFrequenceRepeatFiles
+        {
+            get => isFrequenceRepeatFiles;
+            set
+            {
+                isFrequenceRepeatFiles = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        /// <summary>
+        /// Команда исследования логов
+        /// </summary>
+        public ICommand SearchFrequencyLogsCommand { get; set; }
+
+        public AnalyzeLogsViewModel()
+        {
+            SearchFrequencyLogsCommand = new RelayCommand(obj => SearchFrequencyLogs());
+        }
+
+        /// <summary>
+        /// Анализ лога.
+        /// </summary>
+        private void SearchFrequencyLogs()
+        {
+            if (IsFrequenceRepeatStrings)
+            {
+                if (Logs != null)
+                {
+                    Logs.Clear();
+                }
+                else
+                {
+                    Logs = new ObservableCollection<Log>();
+                }
+                var logs = System.IO.File.ReadLines(LogFilesPath, Encoding.Default);
+                //var aa = System.IO.File.ReadAllText(LogFilesPath, Encoding.Default);
+                string str = "";
+                Dictionary<string, long> dict = new Dictionary<string, long>();
+                foreach (var item in logs)
+                {
+                    str = item.Substring(25);
+                    if (dict.Keys.Contains(str))
+                    {
+                        dict[str]++;
+                    }
+                    else
+                    {
+                        dict.Add(str, 1);
+                    }
+                }
+                var a = from l in dict.OrderByDescending(p => p.Value)
+                        select new Log { MsgLog = l.Key, Count = l.Value };
+                Logs = new ObservableCollection<Log>(a);
+            }
+            if (IsFrequenceRepeatFiles)
+            {
+                if (Logs != null)
+                {
+                    Logs.Clear();
+                }
+                else
+                {
+                    Logs = new ObservableCollection<Log>();
+                }
+                var logs = System.IO.File.ReadLines(LogFilesPath, Encoding.Default);
+                string str = "";
+                Dictionary<string, long> dict = new Dictionary<string, long>();
+                foreach (var item in logs)
+                {
+                    str = Regex.Match(item, @"C:\\.*").Value;
+                    if (dict.Keys.Contains(str))
+                    {
+                        dict[str]++;
+                    }
+                    else
+                    {
+                        dict.Add(str, 1);
+                    }
+                }
+                var a = from l in dict.OrderByDescending(p => p.Value)
+                        select new Log { MsgLog = l.Key, Count = l.Value };
+                Logs = new ObservableCollection<Log>(a);
+            }
+        }
+    }
+}
--- BogdanovCodeAnalyzer/ViewModel/MainWindowViewModel.cs
+++ BogdanovCodeAnalyzer/ViewModel/MainWindowViewModel.cs
@@ -21,208 +21,30 @@ namespace BogdanovCodeAnalyzer.ViewModel
     /// Обработчик формы анализатора
     /// </summary>
     /// <remarks>
-    /// Реализовать следующий функционал по логам:
-    /// 1.1. Помечать файлы, которые не надо покрывать логами;
-    /// 1.2. Помечать методы, которые не надо покрывать логами;
-    /// 1.3. Помечать классы, которые не надо покрывать логами;
-    /// 3. Обработка файла логгирования:
-    /// 3.1. Получение частотности входа в метод;
-    /// 3.2. Получение частотности использования класса;
-    /// 3.3. Получение частотности использования файла;
-    /// 4. Сохранять результаты настроек по обкладке логами кода;
-    /// 5. Сделать логгирование внутри catch;
-    /// 6. Вынести процесс обкладывания логами в отдельную задачу;
-    /// 7. Сделать авторазворачивание инфраструктуры логгирования в проекте:
-    /// 7.1. Подключение необходимых библиотек;
-    /// 7.2. Настройка конфигурационных файлов;
-    /// 7.3. Настройка проекта;
     /// 8. Сравнивать результаты разных логов:
     /// 8.1. Смотреть сходства в логах;
     /// 8.2. Смотреть различия в логах;
     /// 9. Сохранять состояние по объектам:
     /// 9.1. Научиться находить неизменяемую часть состояний объектов;
     /// 9.2. Научиться находить изменяемую часть состояний объектов;
-    /// 10. Научиться удалять обкладку логами без отката системы.
+    /// 11. Научиться анализировать библиотеку:
+    /// 11.1. Научиться строить объектную модель библиотеки (иерархическое дерево);
+    /// 11.2. Научиться находить на иерархическом дереве требуемые типы;
+    /// 11.3. Научиться находить минимальную иерархическую связь между типами;
+    /// 11.4. Научиться находить связь между типами через публичные методы и свойства;
+    /// 11.4.1. Тоже самое, с добавлением других методов и свойств - прайвт, протектод и т.д.;
+    /// 11.4.2. Тоже самое, с учётом того, что эти типы являются наследниками типов более высокого уровня;
+    /// 11.5. Научиться находить вызов типов изнутри методов.
     /// </remarks>
     class MainWindowViewModel : NotifyPropertyChanged
     {
 
-        private string pathToAnalyzeFiles = @"C:\BogdanovR\Experiments\Sintez\";
-        private string textInTextBlock;
-        private string textAddedNamespase = "SintezLibrary";
-        //private string logFilesPath = @"C:\BogdanovR\Experiments\Sintez\SintezOSPClient\bin\Debug\Logs\Log.log";
-        private string logFilesPath = @"C:\BogdanovR\MyReps\BogdanovUtilities\BogdanovCodeAnalyzer\bin\Debug\Logs\Log.log";
-        private bool isFrequenceRepeatStrings;
-        private bool isFrequenceRepeatFiles;
-        private ObservableCollection<Log> logs;
-
-        /// <summary>
-        /// Информация по логам.
-        /// </summary>
-        public ObservableCollection<Log> Logs
-        {
-            get => logs;
-            set
-            {
-                logs = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Путь к файлам, которые будут покрываться логами.
-        /// </summary>
-        public string PathToAnalyzeFiles
-        {
-            get => pathToAnalyzeFiles;
-            set
-            {
-                pathToAnalyzeFiles = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Текст выводимый в текстовый блок.
-        /// </summary>
-        public string TextInTextBlock
-        {
-            get => textInTextBlock;
-            set
-            {
-                textInTextBlock = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Наименование добавляемого пространства имён.
-        /// </summary>
-        public string TextAddedNamespase
-        {
-            get => textAddedNamespase;
-            set
-            {
-                textAddedNamespase = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Путь к анализируемому лог-файлу.
-        /// </summary>
-        public string LogFilesPath
-        {
-            get => logFilesPath;
-            set
-            {
-                logFilesPath = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Флаг того, что ищется частота встречаемости строки в логах
-        /// </summary>
-        public bool IsFrequenceRepeatStrings
-        {
-            get => isFrequenceRepeatStrings;
-            set
-            {
-                isFrequenceRepeatStrings = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Флаг того, что ищется частота встречаемости файлов в логах
-        /// </summary>
-        public bool IsFrequenceRepeatFiles
-        {
-            get => isFrequenceRepeatFiles;
-            set
-            {
-                isFrequenceRepeatFiles = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        /// <summary>
-        /// Команда исследования логов
-        /// </summary>
-        public ICommand SearchFrequencyLogsCommand { get; set; }
 
         public MainWindowViewModel()
         {
-            SearchFrequencyLogsCommand = new RelayCommand(obj => SearchFrequencyLogs());
+            
         }
 
-        /// <summary>
-        /// Анализ лога.
-        /// </summary>
-        private void SearchFrequencyLogs()
-        {
-            if (IsFrequenceRepeatStrings)
-            {
-                if (Logs != null)
-                {
-                    Logs.Clear();
-                }
-                else
-                {
-                    Logs = new ObservableCollection<Log>();
-                }
-                var logs = System.IO.File.ReadLines(LogFilesPath, Encoding.Default);
-                //var aa = System.IO.File.ReadAllText(LogFilesPath, Encoding.Default);
-                string str = "";
-                Dictionary<string, long> dict = new Dictionary<string, long>();
-                foreach (var item in logs)
-                {
-                    str = item.Substring(25);
-                    if (dict.Keys.Contains(str))
-                    {
-                        dict[str]++;
-                    }
-                    else
-                    {
-                        dict.Add(str, 1);
-                    }
-                }
-                var a = from l in dict.OrderByDescending(p => p.Value)
-                        select new Log { MsgLog = l.Key, Count = l.Value };
-                Logs = new ObservableCollection<Log>(a);
-            }
-            if (IsFrequenceRepeatFiles)
-            {
-                if (Logs != null)
-                {
-                    Logs.Clear();
-                }
-                else
-                {
-                    Logs = new ObservableCollection<Log>();
-                }
-                var logs = System.IO.File.ReadLines(LogFilesPath, Encoding.Default);
-                string str = "";
-                Dictionary<string, long> dict = new Dictionary<string, long>();
-                foreach (var item in logs)
-                {
-                    str = Regex.Match(item, @"C:\\.*").Value;
-                    if (dict.Keys.Contains(str))
-                    {
-                        dict[str]++;
-                    }
-                    else
-                    {
-                        dict.Add(str, 1);
-                    }
-                }
-                var a = from l in dict.OrderByDescending(p => p.Value)
-                        select new Log { MsgLog = l.Key, Count = l.Value };
-                Logs = new ObservableCollection<Log>(a);
-            }
-        }
 
     }
 
