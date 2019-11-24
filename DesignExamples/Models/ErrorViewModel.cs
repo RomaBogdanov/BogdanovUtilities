acff0bc8967a2dec9e6c60547a0bbdffcb458cce
rbogdanov <superdiadaroma@rambler.ru>
2019-11-24 22:29:52 +0300

    Добавил вер проект

---
+++ DesignExamples/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesignExamples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
