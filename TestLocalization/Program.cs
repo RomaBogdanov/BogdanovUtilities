bbd49974f180643836b813750f385d41b8371dbb
Roma <superdiadaroma@rambler.ru>
2021-08-22 15:18:13 +0300

    Пробовал локализацию, но видимо не получилось.

---
+++ TestLocalization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLocalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
